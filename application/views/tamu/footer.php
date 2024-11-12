d5cde34693e1db2d3f87a9c5cbe09e896247ac38
Pitriyadi <if23.pitriyadi@mhs.ubpkarawang.ac.id>
2024-11-12 09:55:50 +0700

    first commit

---
+++ application/views/tamu/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer >
+  <div class="container mx-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright © <a href="https://www.sarjanakomedi.com"><?php echo SITE_NAME ."</a> ". Date('Y') ?></span>
+    </div>
+  </div>
+</footer>
