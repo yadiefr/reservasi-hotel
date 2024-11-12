b6e70c88097487707a0c1b18878dfd3a732904f7
Pitriyadi <if23.pitriyadi@mhs.ubpkarawang.ac.id>
2024-11-12 10:59:54 +0700

    first commit

--- application/views/operator/_partials/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Sticky Footer -->
-<footer class="sticky-footer">
-  <div class="container my-auto">
-    <div class="copyright text-center my-auto">
-      <span>Copyright © <a href="https://www.sarjanakomedi.com"><?php echo SITE_NAME ."</a> ". Date('Y') ?></span>
-    </div>
-  </div>
-</footer>
--- assets/script.js
+++
@@ -1,8 +0,0 @@
-  $(document).ready(function() {
-   $("#owl-example").owlCarousel();
-  $('.listing-detail span').tooltip('hide');
-        $('.carousel').carousel({
-            interval: 3000
-        }); 
-        $('.carousel').carousel('cycle');
- }); 
